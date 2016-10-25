e91e1867b7634172bddf4df9a88eb1558b83a647
syafiq <Syafiq,rezpector@gmail.com>
2016-10-25 15:20:38 +0700

    Update matrix inversion testing

--- src/test/java/org/apache/commons/math3/InverseTest.java
+++ src/test/java/org/apache/commons/math3/InverseTest.java
@@ -1,9 +1,9 @@
 package org.apache.commons.math3;
 
-import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.MatrixUtils;
-import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.util.FastMath;
 import org.junit.Test;
 
@@ -16,15 +16,26 @@ public class InverseTest
 {
     @Test public void testMatrix_001()
     {
-        final int nSize = 1000;
-        double[][] m = new double[nSize][nSize];
-        for(int i = -1, is = m.length; ++i < is;)
+        final int  nSize = 355 * 3;
+        double[][] m     = new double[nSize][nSize];
+        for(int i = -1, is = m.length; ++i < is; )
         {
-            for(int j = -1, js = m[i].length; ++j < js;)
+            for(int j = -1, js = m[i].length; ++j < js; )
             {
                 m[i][j] = (i + j + 1) * FastMath.random();
             }
         }
-        RealMatrix pInverse = new LUDecomposition(MatrixUtils.createRealMatrix(m)).getSolver().getInverse();
+        long time = System.currentTimeMillis();
+        new LUDecomposition(MatrixUtils.createRealMatrix(m)).getSolver().getInverse();
+        time = System.currentTimeMillis() - time;
+        long days = TimeUnit.MILLISECONDS.toDays(time);
+        time -= TimeUnit.DAYS.toMillis(days);
+        long hours = TimeUnit.MILLISECONDS.toHours(time);
+        time -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(time);
+        time -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(time);
+        time -= TimeUnit.SECONDS.toMillis(seconds);
+        System.out.printf(Locale.getDefault(), "%02d %02d:%02d:%02d.%03d", days, hours, minutes, seconds, time);
     }
 }
