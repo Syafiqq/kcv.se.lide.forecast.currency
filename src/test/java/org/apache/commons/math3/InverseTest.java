31c15e48df53658904a3f3426a8d2598206b09c8
syafiq <Syafiq,rezpector@gmail.com>
2016-10-25 15:26:59 +0700

    Update matrix inversion testing using supported decomposition method

--- src/test/java/org/apache/commons/math3/InverseTest.java
+++ src/test/java/org/apache/commons/math3/InverseTest.java
@@ -2,9 +2,14 @@ package org.apache.commons.math3;
 
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.math3.linear.CholeskyDecomposition;
+import org.apache.commons.math3.linear.EigenDecomposition;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.QRDecomposition;
+import org.apache.commons.math3.linear.SingularValueDecomposition;
 import org.apache.commons.math3.util.FastMath;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -14,10 +19,12 @@ import org.junit.Test;
  */
 public class InverseTest
 {
-    @Test public void testMatrix_001()
+    private double[][] m;
+
+    @Before public void initialization()
     {
         final int  nSize = 355 * 3;
-        double[][] m     = new double[nSize][nSize];
+        this.m     = new double[nSize][nSize];
         for(int i = -1, is = m.length; ++i < is; )
         {
             for(int j = -1, js = m[i].length; ++j < js; )
@@ -25,8 +32,45 @@ public class InverseTest
                 m[i][j] = (i + j + 1) * FastMath.random();
             }
         }
+    }
+
+    @Test public void LUDecompositionTesting()
+    {
         long time = System.currentTimeMillis();
-        new LUDecomposition(MatrixUtils.createRealMatrix(m)).getSolver().getInverse();
+        new LUDecomposition(MatrixUtils.createRealMatrix(this.m)).getSolver().getInverse();
+        System.out.println(this.parseTime(time));
+    }
+
+    @Test public void CholeskyDecompositionTesting()
+    {
+        long time = System.currentTimeMillis();
+        new CholeskyDecomposition(MatrixUtils.createRealMatrix(this.m)).getSolver().getInverse();
+        System.out.println(this.parseTime(time));
+    }
+
+    @Test public void QRDecompositionTesting()
+    {
+        long time = System.currentTimeMillis();
+        new QRDecomposition(MatrixUtils.createRealMatrix(this.m)).getSolver().getInverse();
+        System.out.println(this.parseTime(time));
+    }
+
+    @Test public void EDecompositionTesting()
+    {
+        long time = System.currentTimeMillis();
+        new EigenDecomposition(MatrixUtils.createRealMatrix(this.m)).getSolver().getInverse();
+        System.out.println(this.parseTime(time));
+    }
+
+    @Test public void SVDecompositionTesting()
+    {
+        long time = System.currentTimeMillis();
+        new SingularValueDecomposition(MatrixUtils.createRealMatrix(this.m)).getSolver().getInverse();
+        System.out.println(this.parseTime(time));
+    }
+
+    private String parseTime(long time)
+    {
         time = System.currentTimeMillis() - time;
         long days = TimeUnit.MILLISECONDS.toDays(time);
         time -= TimeUnit.DAYS.toMillis(days);
@@ -36,6 +80,6 @@ public class InverseTest
         time -= TimeUnit.MINUTES.toMillis(minutes);
         long seconds = TimeUnit.MILLISECONDS.toSeconds(time);
         time -= TimeUnit.SECONDS.toMillis(seconds);
-        System.out.printf(Locale.getDefault(), "%02d %02d:%02d:%02d.%03d", days, hours, minutes, seconds, time);
+        return String.format(Locale.getDefault(), "%02d %02d:%02d:%02d.%03d", days, hours, minutes, seconds, time);
     }
 }
